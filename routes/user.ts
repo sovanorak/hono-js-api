10246a0e8cb960b96439467a6dff3139ab64a37d
SOVANORAK <norakeusever@gmail.com>
2025-02-21 14:56:25 +0700

    installed bcryptjs

--- routes/user.ts
+++ routes/user.ts
@@ -1,19 +1,21 @@
 import { Hono } from "hono";
-import { z } from "zod";
+
 import { PrismaClient } from "@prisma/client";
 import { zValidator } from "@hono/zod-validator";
 import { UserSchema } from "../schemas";
+import * as bcrypt from "bcryptjs";
 
 const app = new Hono();
 const prisma = new PrismaClient();
 
 app.post("/", zValidator("json", UserSchema), async (c) => {
   const { email, name, password } = c.req.valid("json");
+  const hashedPassword = await bcrypt.hashSync(password, 10);
   const user = await prisma.user.create({
     data: {
       email,
       name,
-      password,
+      password: hashedPassword,
     },
   });
   return c.json({ user });
