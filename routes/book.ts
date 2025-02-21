37bf2591f6874a1160e6b563eee289e75ee98764
SOVANORAK <norakeusever@gmail.com>
2025-02-21 10:32:27 +0700

    get multiple params

--- routes/book.ts
+++ routes/book.ts
@@ -7,4 +7,9 @@ app.get("/", (c) => c.json("list books"));
 app.post("/", (c) => c.json("create a book", 201));
 app.get("/:id", (c) => c.json(`get ${c.req.param("id")}`));
 
+app.get("/:id/comment/:comment_id", async (c) => {
+  const { id, comment_id } = c.req.param();
+  return c.json(`book_id: ${id} comment_id: ${comment_id}`);
+});
+
 export default app;
