97805d595889cca08dec16a1673a4133839dcd09
SOVANORAK <norakeusever@gmail.com>
2025-02-21 15:43:07 +0700

    pagination query user

--- routes/user.ts
+++ routes/user.ts
@@ -8,6 +8,7 @@ import * as bcrypt from "bcryptjs";
 const app = new Hono();
 const prisma = new PrismaClient();
 
+//Create user
 app.post("/", zValidator("json", UserSchema), async (c) => {
   const { email, name, password } = c.req.valid("json");
   const hashedPassword = await bcrypt.hashSync(password, 10);
@@ -19,6 +20,22 @@ app.post("/", zValidator("json", UserSchema), async (c) => {
     },
   });
   return c.json({ user });
+});
+
+//Find all users
+app.get("/", async (c) => {
+  //pagination
+  const page = Number(c.req.query("page") || 1);
+  const limit = Number(c.req.query("limit") || 10);
+
+  const defaultPaginate = {
+    skip: (page - 1) * limit, // offset formula
+    take: limit,
+  };
+
+  const data = await prisma.user.findMany(defaultPaginate);
+  const count = await prisma.user.count(defaultPaginate);
+  return c.json({ data, count });
 });
 
 export default app;
